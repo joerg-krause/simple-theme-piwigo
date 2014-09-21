edda7ff54409541963226cd88e53f19661650f15
Jörg Krause <jkrause@posteo.de>
2014-09-21 14:35:06 +0200

    themeconf.inc.php: Change themeconf

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,24 +1,24 @@
 <?php
 /*
-Theme Name: SimpleNG
-Version: 3.2.1
+Theme Name: SimpleNG3
+Version: 4.0.0
 Description: SimpleNG theme for Piwigo
 Theme URI: http://piwigo.org/ext/extension_view.php?eid=602
-Author: Saïmon
-Author URI: http://saimon.org/
+Author: Jörg
+Author URI: http://embedded.rocks/
 */
 
 $themeconf = array(
   'parent' => 'default',
   'load_parent_css' => false,
   'load_parent_local_head' => false,
-  'name' => 'simpleng',
-  'theme_dir' => 'simpleng',
+  'name' => 'simpleng3',
+  'theme_dir' => 'simpleng3',
   'icon_dir' => 'themes/default/icon',
   'img_dir'      => 'themes/default/images',
   'mime_icon_dir' => 'themes/default/icon/mimetypes/',
   'local_head' => 'local_head.tpl',
-  'url' => 'http://saimon.org/simple-theme-piwigo/'
+  'url' => 'http://embedded.rocks/simpleng3-theme-piwigo/'
 );
 
 // set this if you want to restrict the width of pictures to a specifc width
